40cb33b1ce26c57c7bc7c5397ad3cda39a31674c
sandippoddar <sandip.poddar@innoraft.com>
2024-10-03 15:29:09 +0530

    FT2-313: Fixed Responsive issues.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(document).ready(()=>{
-    $(".hamberger").click(function() {
-        $(".menu").show();
-    })
+$(document).ready(() => {
+
+    $(".hamberger").click(function(event) {
+        event.stopPropagation();
+        $(".menu").slideDown(300);
+    });
+
     $(".close").click(function() {
-        $(".menu").hide();
-    })
-})
+        $(".menu").slideUp(300);
+    });
+
+    const specificDiv = document.querySelector('.menu');
+
+    document.addEventListener('click', function(event) {
+        if (!specificDiv.contains(event.target) && !$(".hamberger").is(event.target)) {
+            $(".menu").slideUp(300);
+        }
+    });
+});
